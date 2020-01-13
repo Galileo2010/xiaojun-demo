61912298ea0ff9d43990294c0e52bf74bb1fbdfb
Xiaojun Luo <xiaojun.luo@thoughtworks.com>
2020-01-13 11:54:46 +0800

    rename

--- xiaojun/views.py
+++ xiaojun/views.py
@@ -3,9 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def get(request):
-  data = request.GET['data']
-  print(data)
-  return HttpResponse("hello world %s." % data)
+  name = request.GET['name']
+  print(name)
+  return HttpResponse("hello world %s." % name)
 
 def post(request):
   data = request.body
